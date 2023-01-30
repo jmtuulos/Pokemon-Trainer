1a73ce71a9a71f50371bd132fc4e999f8587220f
SethOberg <seob95@gmail.com>
2023-01-30 16:51:59 +0100

    Added pokemon service

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const { apiTrainers, apiKey } = environment;
+
+const httpRequestHeaders = new HttpHeaders({
+  'Content-type': 'application/json',
+  'x-api-key': apiKey,
+});
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonService {
+  constructor(private readonly http: HttpClient) {}
 
-  constructor() { }
+  // public getPokemon(): Observable<Pokemon> {}
 }
