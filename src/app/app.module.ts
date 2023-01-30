85e6f4f305cd3d8f8510cea213756c4117c571f9
SethOberg <seob95@gmail.com>
2023-01-30 16:21:48 +0100

    Feat: started working on Catalogue page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,7 @@ import { CataloguePage } from './pages/catalogue/catalogue.page';
 
 @NgModule({
   declarations: [AppComponent, LoginPage, TrainerPage, CataloguePage],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
