3b41f26d3e4e98935357a13255636722b3be2dc0
SethOberg <seob95@gmail.com>
2023-01-30 20:29:14 +0100

    Added components for pokemon list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,18 @@ import { AppComponent } from './app.component';
 import { LoginPage } from './pages/login/login.page';
 import { TrainerPage } from './pages/trainer/trainer.page';
 import { CataloguePage } from './pages/catalogue/catalogue.page';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonListItemComponent } from './components/pokemon-list-item/pokemon-list-item.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginPage, TrainerPage, CataloguePage],
+  declarations: [
+    AppComponent,
+    LoginPage,
+    TrainerPage,
+    CataloguePage,
+    PokemonListComponent,
+    PokemonListItemComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
