49fdbbee57a3a053bd3a81cb5d5d41213f881744
Juha <viderus@gmail.com>
2023-01-31 18:17:06 +0200

    added logging in functionalities to login services

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable, of, switchMap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user.models';
+
+const { apiTrainers, apiTrainersKey } = environment
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private readonly http: HttpClient) {}
+  public login(username: string): Observable<User> {
+    return this.checkUsername(username)
+      .pipe(
+        switchMap((user: User | undefined) => {
+          if (user === undefined) {
+            return this.createUser(username)
+          }
+          return of(user)
+        })
+      )
+  }
+  
+  //Check if user exists
+  private checkUsername(username: string): Observable<User | undefined> {
+    return this.http.get<User[]>(`${apiTrainers}?username=${username}`)
+    .pipe(
+      map((response: User[]) => response.pop())
+    )
+  }
+
+  //Create user
+  private createUser(username: string): Observable<User> {
+    const user = {
+      username,
+      pokemons: []
+    }
+
+    const headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "x-api-key": apiTrainersKey
+    })
+    return this.http.post<User>(apiTrainers, user, {
+      headers
+    })
+  }
+
+}
