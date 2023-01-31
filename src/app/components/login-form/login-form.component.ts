3bc3eed487faf16e27b090024f003ce6d9b83316
Juha <viderus@gmail.com>
2023-01-31 18:11:16 +0200

    added user model and login-form component

---
+++ src/app/components/login-form/login-form.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/models/user.models';
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent{
+  constructor(private readonly loginService: LoginService) {}
+
+  public loginSubmit(loginForm: NgForm): void {
+
+    //username!
+    const { username } = loginForm.value
+    this.loginService.login(username)
+      .subscribe({
+        next: (user: User) => {
+
+        },
+        error: () => {
+
+        }
+      })
+  }
+}
