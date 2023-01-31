7eefcb53f5fe103c32fb98b2b67360ba549c5eb2
Juha <viderus@gmail.com>
2023-01-31 18:16:19 +0200

    added the loginForm modules and components (work in progress)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { LoginPage } from './pages/login/login.page';
 import { TrainerPage } from './pages/trainer/trainer.page';
 import { CataloguePage } from './pages/catalogue/catalogue.page';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, LoginPage, TrainerPage, CataloguePage],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, LoginPage, TrainerPage, CataloguePage, LoginFormComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
