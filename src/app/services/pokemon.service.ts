c9bb28a4322cccbefc342a9c25ef90c95a22a2ab
SethOberg <seob95@gmail.com>
2023-01-31 15:33:14 +0100

    Get pokemon from API

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,7 +1,18 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Pokemon } from '../models/pokemon.model';
+
+const pokeApiUrl = 'https://pokeapi.co/api/v2';
+const pokemonEndpoint = '/pokemon?limit=10&offset=0';
+const imgUrl =
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const fileEnding = '.png';
 
 const { apiTrainers, apiKey } = environment;
 
@@ -16,5 +27,31 @@ const httpRequestHeaders = new HttpHeaders({
 export class PokemonService {
   constructor(private readonly http: HttpClient) {}
 
-  // public getPokemon(): Observable<Pokemon> {}
+  private _listOfPokemon = [];
+
+  get listOfPokemon(): any {
+    return this._listOfPokemon;
+  }
+
+  public fetchAllPokemon(): void {
+    this.http.get<any>(pokeApiUrl + pokemonEndpoint).subscribe(
+      (data: any) => {
+        this.mapResultsToPokemon(data.results);
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
+  }
+
+  public mapResultsToPokemon(data: any): void {
+    for (let i = 0; i < data.length; i++) {
+      //pokemon IDs start at 1, therefore add 1
+      let id = i + 1;
+      let name = data[i].name;
+      let image: string = imgUrl + id + fileEnding;
+      const newPokemon = { id, name, image } as Pokemon;
+      this.listOfPokemon.push(newPokemon);
+    }
+  }
 }
