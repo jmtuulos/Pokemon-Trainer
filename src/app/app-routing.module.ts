74c73323ef43c740a1db67508588a31f432e93a0
Juha <viderus@gmail.com>
2023-02-01 12:15:26 +0200

    Added guarding to catalogue and trainer pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { CataloguePage } from './pages/catalogue/catalogue.page';
 import { LoginPage } from './pages/login/login.page';
 import { TrainerPage } from './pages/trainer/trainer.page';
@@ -7,8 +8,8 @@ import { TrainerPage } from './pages/trainer/trainer.page';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/login'},
   { path: 'login', component: LoginPage },
-  { path: 'trainer', component: TrainerPage },
-  { path: 'catalogue', component: CataloguePage },
+  { path: 'trainer', component: TrainerPage, canActivate: [ AuthGuard] },
+  { path: 'catalogue', component: CataloguePage, canActivate: [ AuthGuard ] },
 ];
 
 @NgModule({
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private readonly router: Router,
+    private readonly userService: UserService
+  ) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.userService.user) {
+      return true
+    }
+    this.router.navigateByUrl("/login")
+    return false
+  }
+
+}
